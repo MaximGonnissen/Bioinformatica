c2c0f564ed12bc8e7713a0ad42b963fa68f0c4b5
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-05-25 17:23:57 +0200

    Add typehints and boolean return to check_and_create_dir

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,14 @@
 import os
 
 
-def check_and_create_dir(path):
+def check_and_create_dir(path: str) -> bool:
+    """
+    Check if directory exists, create it if not
+    :param path: Path to directory
+    :return: True if directory was created, False if directory already exists
+    """
     path = os.path.dirname(path)
     if not os.path.exists(path):
         os.makedirs(path)
+        return True
+    return False
