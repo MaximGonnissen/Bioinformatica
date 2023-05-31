e050e3cb23df46cd546cdec29bba05069894445b
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-05-31 22:46:09 +0200

    Expand tests
    Signed-off-by: MaximG <maxim.gonnissen@student.uantwerpen.be>

--- tests/psa_tests/test_needleman_wunsch.py
+++ tests/psa_tests/test_needleman_wunsch.py
@@ -39,3 +39,59 @@ class TestNeedlemanWunsch(unittest.TestCase):
 
         for alignment in alignments:
             self.assertIn(alignment, correct_pairs)
+
+    def test_needleman_wunsch_2(self):
+        """
+        Test the Needleman-Wunsch algorithm with an indel penalty of 1.
+        """
+        sequences = parse("psa_tests/test_inputs/test.fasta")
+
+        sequence_ids = list(sequences.keys())
+        sequence2 = sequences[sequence_ids[1]]
+        sequence1 = sequences[sequence_ids[0]]
+
+        config = {
+            'indel': 3
+        }
+
+        solver = NeedlemanWunschPSASolver(config=config, substitution_matrix=BLOSUM(62))
+
+        score, alignments = solver.solve(sequence_1=sequence1, sequence_2=sequence2)
+
+        self.assertEqual(score, -11)
+        self.assertEqual(len(alignments), 1)
+
+        correct_pairs = [
+            ("GYSSA--SKIIF", "N-TEAFFGQGT-")
+        ]
+
+        for alignment in alignments:
+            self.assertIn(alignment, correct_pairs)
+
+    def test_needleman_wunsch_3(self):
+        """
+        Test the Needleman-Wunsch algorithm with an indel penalty of 1.
+        """
+        sequences = parse("psa_tests/test_inputs/test.fasta")
+
+        sequence_ids = list(sequences.keys())
+        sequence2 = sequences[sequence_ids[1]]
+        sequence1 = sequences[sequence_ids[0]]
+
+        config = {
+            'indel': 15
+        }
+
+        solver = NeedlemanWunschPSASolver(config=config, substitution_matrix=BLOSUM(62))
+
+        score, alignments = solver.solve(sequence_1=sequence1, sequence_2=sequence2)
+
+        self.assertEqual(score, -16)
+        self.assertEqual(len(alignments), 1)
+
+        correct_pairs = [
+            ("GYSSASKIIF", "NTEAFFGQGT")
+        ]
+
+        for alignment in alignments:
+            self.assertIn(alignment, correct_pairs)
