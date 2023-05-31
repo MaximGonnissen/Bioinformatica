ad76895a2d939a35fb7737003de1195b43ee128f
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-06-01 01:51:23 +0200

    Initial SW & NW MSA versions
    Signed-off-by: MaximG <maxim.gonnissen@student.uantwerpen.be>

---
+++ src/msa/needleman_wunsch.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+from msa.msa_solver import MSASolver
+from psa.needleman_wunsch import NeedlemanWunschPSASolver
+from psa.psa_solver import PSASolver
+
+
+class NeedlemanWunschMSASolver(MSASolver):
+    """
+    Class for the Needleman-Wunsch multiple sequence alignment solver.
+    """
+
+    @property
+    def solver_cls(self) -> Callable[[dict, dict], PSASolver]:
+        """
+        Returns the solver class.
+        :return: The solver class.
+        """
+        return NeedlemanWunschPSASolver
