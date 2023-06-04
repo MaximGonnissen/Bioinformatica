978f3c3dc933eaa5f188c82f96343835759571f2
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-06-04 14:51:02 +0200

    Start of Smith Waterman MSA traceback
    Signed-off-by: MaximG <maxim.gonnissen@student.uantwerpen.be>

--- src/msa/smith_waterman.py
+++ src/msa/smith_waterman.py
@@ -1,7 +1,5 @@
 from typing import List, Tuple, Union
 
-import numpy as np
-
 from msa.msa_solver import MSASolver
 from msa.scoring_matrix.scoring_matrix import ScoringMatrix
 
@@ -50,5 +48,25 @@ class SmithWatermanMSASolver(MSASolver):
         for index in self.scoring_matrix.iter_non_zero_indices():
             self.scoring_matrix.set_score(*index, score=self.scoring_function(*index))
 
-    def traceback(self) -> Tuple[Union[int, float], List[str]]:
-        pass
+    def traceback(self, *args) -> List[Tuple[str, ...]]:
+        """
+        Perform the traceback.
+        :param args: Coordinates to start the traceback from.
+        :return: List of tuples of aligned sequences.
+        """
+        if self.scoring_matrix.is_zero_index(*args):
+            return []
+
+        if len(self.scoring_matrix.get_traceback(*args)) == 0:
+            return []  # TODO: May need to return sequence chars instead of nothing, in this case?
+
+        new_alignments = []
+        for traceback_direction in self.scoring_matrix.get_traceback(*args):
+            traceback_alignments = self.traceback(*traceback_direction)
+            for alignment in traceback_alignments:
+                new_alignment = []
+                for i in range(len(alignment)):
+                    new_alignment.append(alignment[i] + self.scoring_matrix.sequences[i][args[i] - 1])
+                new_alignments.append(tuple(new_alignment))
+
+        return new_alignments
