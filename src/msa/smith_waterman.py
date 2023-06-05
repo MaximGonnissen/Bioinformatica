2bc45b01972e5b605878482122fa82e0d03ef876
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-06-05 16:14:52 +0200

    Move scoring function to MSASolver (part 2)
    Signed-off-by: MaximG <maxim.gonnissen@student.uantwerpen.be>

--- src/msa/smith_waterman.py
+++ src/msa/smith_waterman.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Union
+from typing import List, Tuple
 
 from msa.msa_solver import MSASolver
 from msa.scoring_matrix.scoring_matrix import ScoringMatrix
@@ -16,30 +16,6 @@ class SmithWatermanMSASolver(MSASolver):
         :return: Initialised scoring matrix.
         """
         return ScoringMatrix(sequences)
-
-    def scoring_function(self, *args) -> Union[int, float]:
-        """
-        Scoring function for the MSA solver.
-        :param args: Coordinates for the scoring matrix.
-        :return: Score for the given coordinates.
-        """
-        # Simple version for now, only taking into account matches and mismatches.
-        indices = args
-        if len(indices) != len(self.scoring_matrix.shape):
-            raise IndexError("Incorrect number of indices given.")
-
-        # Get the indices for the sequences.
-        sequence_chars = [self.scoring_matrix.sequences[i][indices[i] - 1] for i in range(len(indices))]
-
-        score = 0
-        used_chars = []
-        for char in sequence_chars:
-            for other_char in sequence_chars:
-                if char != other_char and char not in used_chars:
-                    score += self.substitution_matrix[char][other_char]
-            used_chars.append(char)
-
-        return score
 
     def fill_scoring_matrix(self):
         """
