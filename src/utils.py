0791c6e2dbd255b54b851bdd8d0501905d9002a2
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-06-05 23:45:07 +0200

    Create print lined up alignments method
    Signed-off-by: MaximG <maxim.gonnissen@student.uantwerpen.be>

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,29 @@ def check_and_create_dir(path: str) -> bool:
         os.makedirs(path)
         return True
     return False
+
+
+def print_lined_up_alignments(alignments: list[tuple[str, ...]]) -> None:
+    """
+    Print a list of alignments lined up.
+
+    Legend: * to indicate a match, : to indicate a mismatch, space to indicate a gap
+
+    :param alignments: The alignments to print.
+    """
+    for index, alignment in enumerate(alignments):
+        print(f"Alignment {index + 1}:")
+        prev_alignment = alignment[0]
+        print(f"\t{prev_alignment}")
+        for i in range(1, len(alignment)):
+            current_alignment = alignment[i]
+            alignment_comparison = ""
+            for j in range(len(current_alignment)):
+                if current_alignment[j] == prev_alignment[j]:
+                    alignment_comparison += "*"
+                elif current_alignment[j] == "-" or prev_alignment[j] == "-":
+                    alignment_comparison += " "
+                else:
+                    alignment_comparison += ":"
+            print(f"\t{alignment_comparison}\n\t{current_alignment}")
+            prev_alignment = current_alignment
