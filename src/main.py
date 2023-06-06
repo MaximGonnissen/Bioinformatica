e04f8a45acc5aa3641dd34e893da06e2cf64f2d7
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-06-06 17:13:15 +0200

    Add CLI to main
    Signed-off-by: MaximG <maxim.gonnissen@student.uantwerpen.be>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import argparse
 import json
 
-from utils import check_and_create_dir
+from src.fasta_parser.fasta_parser import parse
+from src.msa.needleman_wunsch import NeedlemanWunschMSASolver
+from src.msa.smith_waterman import SmithWatermanMSASolver
+from src.psa.needleman_wunsch import NeedlemanWunschPSASolver
+from src.psa.smith_waterman import SmithWatermanPSASolver
+from src.utils import check_and_create_dir
 
 if __name__ == '__main__':
     # Create argument parser
@@ -9,8 +14,28 @@ if __name__ == '__main__':
 
     # Add arguments
     parser.add_argument('-c', '--config', help='Path to config file', type=str, default='./data/config/config.json')
-    parser.add_argument('-i', '--input', help='Path to input file', type=str, default='./data/input/input.fasta')
+    parser.add_argument('-i', '--input', help='Path to input file', type=str,
+                        default='./data/input/cs_assignment.fasta')
     parser.add_argument('-o', '--output', help='Path to output file', type=str, default='./data/output/output.txt')
+
+    # Add subparsers for pairwise and multiple sequence alignment
+    subparsers = parser.add_subparsers(dest='mode', help='Alignment mode', required=True)
+    pairwise_parser = subparsers.add_parser('pairwise', help='Pairwise sequence alignment')
+    msa_parser = subparsers.add_parser('msa', help='Multiple sequence alignment')
+
+    # Add subparsers for pairwise alignment
+    pairwise_subparsers = pairwise_parser.add_subparsers(dest='pairwise_mode', help='Pairwise alignment mode',
+                                                         required=True)
+    needleman_wunsch_parser = pairwise_subparsers.add_parser('needleman_wunsch',
+                                                             help='Needleman-Wunsch pairwise alignment')
+    smith_waterman_parser = pairwise_subparsers.add_parser('smith_waterman', help='Smith-Waterman pairwise alignment')
+
+    # Add subparsers for multiple sequence alignment
+    msa_subparsers = msa_parser.add_subparsers(dest='msa_mode', help='Multiple sequence alignment mode', required=True)
+    needleman_wunsch_msa_parser = msa_subparsers.add_parser('needleman_wunsch',
+                                                            help='Needleman-Wunsch multiple sequence alignment')
+    smith_waterman_msa_parser = msa_subparsers.add_parser('smith_waterman',
+                                                          help='Smith-Waterman multiple sequence alignment')
 
     # Parse arguments
     args = parser.parse_args()
@@ -22,4 +47,42 @@ if __name__ == '__main__':
     config = json.load(open(args.config, 'r'))
 
     # Run program
-    # TODO: Run program
+    solver = None
+
+    if args.mode == 'pairwise':
+        if args.pairwise_mode == 'needleman_wunsch':
+            solver = NeedlemanWunschPSASolver(config)
+        elif args.pairwise_mode == 'smith_waterman':
+            solver = SmithWatermanPSASolver(config)
+        else:
+            raise ValueError('Invalid pairwise alignment mode')
+
+    elif args.mode == 'msa':
+        if args.msa_mode == 'needleman_wunsch':
+            solver = NeedlemanWunschMSASolver(config)
+        elif args.msa_mode == 'smith_waterman':
+            solver = SmithWatermanMSASolver(config)
+        else:
+            raise ValueError('Invalid multiple sequence alignment mode')
+
+    else:
+        raise ValueError('Invalid alignment mode')
+
+    sequence_info = parse(args.input)
+    sequence_values = [sequence_info[key] for key in sequence_info.keys()]
+
+    score, alignments = None, []
+
+    if args.mode == 'pairwise':
+        score, alignment = solver.solve(sequence_values[0], sequence_values[1])
+    else:
+        score, alignments = solver.solve(sequence_values)
+
+    print('Alignments score: {}'.format(score))
+    print('Alignments written to {}'.format(args.output))
+
+    with open(args.output, 'w') as f:
+        for alignment in alignments:
+            for i in range(len(alignment)):
+                alignment_id = list(sequence_info.keys())[i]
+                f.write(f"{alignment_id}: {alignment[i]}\n")
